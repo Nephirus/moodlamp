cd31a15283becef785518cc8e6a3d7bbe4d96104
Nephirus <nephirus@kerestes.cz>
2012-07-18 00:57:17 +0200

    random.c - speed modes

--- random.c
+++ random.c
@@ -28,10 +28,17 @@ volatile uint8_t dr, dg, db;
 volatile uint8_t phase = 0;
 volatile uint8_t cnt=0;
 
-volatile uint8_t threshold = 100;
+volatile uint8_t threshold;
 
-ISR (TIM1_OVF_vect)        
-{
+volatile uint8_t input1 = 1;
+volatile uint8_t input2 = 1;
+volatile uint8_t input1_cnt=0;
+volatile uint8_t input2_cnt=0;
+
+const uint8_t thr[6] = {1,2,5,10,50,200};
+
+
+ISR (TIM1_OVF_vect){
 	
 	if(++cnt>=threshold){
 		switch (phase){
@@ -72,13 +79,28 @@ ISR (TIM1_OVF_vect)
 		OCR0A = r;
 		cnt = 0;
 	}
+	
 }
+
 
 int main(void){
 	init();
+	uint8_t i=4;
+	threshold = thr[i];
 
 	while(1){
-		sleep_mode();
+		if(((PINB & _BV(PB2))) == 0){
+			if(i>0){
+				threshold = thr[--i];
+			}
+			_delay_ms(500);
+		}
+		if(((PINB & _BV(PB3))) == 0){
+			if(i<5){
+				threshold = thr[++i];
+			}
+			_delay_ms(500);
+		}
 	}
 	return 0;
 }
@@ -86,7 +108,7 @@ int main(void){
 void init(void){
 	/* green - OCR1A - PB1 */
 	TCCR1 = _BV(PWM1A) | _BV(COM1A1);
-    TCCR1 |= _BV(CS10);
+    TCCR1 |= _BV(CS11);
     OCR1A = 0;
     DDRB |= _BV (PB1);
 	
@@ -97,10 +119,12 @@ void init(void){
 	
 	/* red - OCR0A - PB0 */
 	TCCR0A = _BV(COM0A1) | _BV(WGM00) | _BV(WGM01);
-	TCCR0B = _BV(CS00);
+	TCCR0B = _BV(CS01);
 	OCR0A = 0;
 	DDRB |= _BV(PB0);
 
+	/* input buttons */
+	PORTB |= _BV(PB2) | _BV(PB3);
 	
     TIMSK = _BV (TOIE1);
     sei ();
